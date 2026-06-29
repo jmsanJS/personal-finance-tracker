22994990273714f27ff5fef26544282cc2ee0926
Juan Sanchez <juanboca90@gmail.com>
2026-06-29 23:01:10 +0200

    feat(frontend): add categories page with reactive form, list, and color dot

--- frontend/src/app/pages/categories/categories.ts
+++ frontend/src/app/pages/categories/categories.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Category, CategoryService } from '../../core/services/category';
+import { AuthService } from '../../core/services/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './categories.html',
   styleUrl: './categories.scss',
 })
-export class Categories {}
+export class Categories implements OnInit {
+  categories = signal<Category[]>([]);
+  showForm = false;
+  loading = signal(true);
+  error = signal('');
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+    type: new FormControl<'income' | 'expense'>('expense', Validators.required),
+    color: new FormControl('#6366f1', Validators.required),
+  });
+
+  constructor(
+    public authService: AuthService,
+    private categoryService: CategoryService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryService.getAll().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+        this.loading.set(false);
+      },
+      error: () => this.loading.set(false),
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    this.error.set('');
+
+    const { name, type, color } = this.form.value;
+
+    this.categoryService
+      .create({
+        name: name!,
+        type: type!,
+        color: color!,
+      })
+      .subscribe({
+        next: (c) => {
+          this.categories.set([c, ...this.categories()]);
+          this.form.reset({ type: 'expense' });
+          this.showForm = false;
+        },
+        error: () => this.error.set('Failed to create category'),
+      });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
